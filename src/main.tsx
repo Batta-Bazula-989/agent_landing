e1040d4f3f0034d2ed5a6e8813484fa1b7ee0c7e
RomanZakharenkoUa <115548670+RomanZakharenkoUa@users.noreply.github.com>
2025-11-16 13:52:00 +0100

    fixed UI LOADING 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-// Styles are loaded early via <link rel="stylesheet"> in index.html to prevent FOUC
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
